9bcc539d87777b9f093ef4c41b1c4ca90431ff44
shohan15020 <mdshohansajjad@gmail.com>
2024-02-18 17:40:58 +0600

    modal made

--- js/make.js
+++ js/make.js
@@ -9,7 +9,15 @@ const applyBtn = document.getElementById('apply');
 for(let btn of allBtn){
     // console.log(btn);
     // seat count section
+    
     btn.addEventListener('click', function(event){
+        
+        // you cannot select more than 4
+        if(count >= 4){
+            alert('You cannot selected more then 4 ticket')
+            return;
+        }
+
         count = count + 1;
         const seatCount = document.getElementById('seat-count');
         seatCount.innerText = count;
@@ -67,13 +75,30 @@ for(let btn of allBtn){
             applyBtn.classList.add('hide');
         }
         
+        // next button 
+        const nextButton = document.getElementById('nextButton')
+        console.log(nextButton);
+
+        const phoneNumber = document.getElementById('phoneNumber').value;
+
+        if (count !== 0   && phoneNumber != '') {
+            nextButton.classList.remove('hide');
+        } else {
+            nextButton.classList.add('hide');
+        }
+        
+
+        // button disable if clicked again
+        if (!btn.disabled) {
+            btn.disabled = true;
+        }
+        
         
     })
 }
 
 // coupon code apply 
-// NEW15
-// Coupon Code
+
 const apply = document.getElementById('apply');
 apply.addEventListener('click', function(){
     const couponElement = document.getElementById('input-field').value;
@@ -87,7 +112,16 @@ apply.addEventListener('click', function(){
         grandPriceElement.innerText = discountGrantTotal;
          // clear value of input field
          document.getElementById('input-field').value = '';
-    }else{
+    }else if(convertCoupon === 'COUPLE20'){
+        // grand total
+        const grandPriceElement = document.getElementById('grand');
+        const grantTotal = totalPrice * 0.2;
+        const discountGrantTotal = totalPrice - grantTotal ;
+        grandPriceElement.innerText = discountGrantTotal;
+         // clear value of input field
+         document.getElementById('input-field').value = '';
+    }
+    else{
         alert('Invalid Coupon code')
             // clear value of input field
         document.getElementById('input-field').value = '';
@@ -105,12 +139,3 @@ function setInnerText(id, value) {
 function getInnerText(id, value) {
     document.getElementById(id).innerText = value;
 }
-// 
-// function totalCost(id, value) {
-//     let totalCost = document.getElementById(id).innerText;
-//     let ConvertedTotalCost = parseInt(totalCost);
-
-
-//     let sum = ConvertedTotalCost + parseInt(value);
-//     setInnerText(id, sum);
-// }
