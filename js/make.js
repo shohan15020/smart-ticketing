bd32316a1692cfeceeeb59fa4a9206e8b347dfde
shohan15020 <mdshohansajjad@gmail.com>
2024-02-19 14:39:18 +0600

    discount price section create

--- js/make.js
+++ js/make.js
@@ -38,10 +38,13 @@ for(let btn of allBtn){
         const li = document.createElement('ul');
         li.style.display = 'flex';
         li.style.justifyContent = 'space-between';
+
         const p = document.createElement('p');
         p.innerText = seatNo;
+
         const p1 = document.createElement('p');
         p1.innerText = "Economy";
+
         const p2 = document.createElement('p');
         p2.innerText = "550";
         let price = p2.innerText;
@@ -52,37 +55,46 @@ for(let btn of allBtn){
         
         ticketShow.appendChild(li);
         
-        console.log(typeof totalPrice);
+        // console.log(typeof totalPrice);
         
-        totalPrice = totalPrice + parseInt(price);
-        console.log(totalPrice);
+        totalPrice = totalPrice + parseFloat(price);
         // console.log(totalPrice);
+        // // console.log(totalPrice);
 
         const totalPriceElement = document.getElementById('total-cost');
         totalPriceElement.innerText = totalPrice;
         
         // coupon code
         // Check if 4 seats are selected, then enable the coupon code button
-        
+        const inputDiv = document.getElementById('input-div');
+        const applyBtn = document.getElementById('apply');
+        const inputField = document.getElementById('input-field');
         if (count === 4) {
             applyBtn.classList.remove('hide');
+            inputField.classList.remove('hide');
+            // applyBtn.classList.remove('text-white');
+            applyBtn.classList.add('bg-[#1DD100]');
+            applyBtn.classList.add('text-white');
         } else {
-            applyBtn.classList.add('hide');
+            // applyBtn.classList.add('hide');
         }
         
         // next button 
         const nextButton = document.getElementById('nextButton')
-        console.log(nextButton);
+        
 
         const phoneNumber = document.getElementById('phoneNumber').value;
 
         if (count !== 0   && phoneNumber != '') {
             nextButton.classList.remove('hide');
+            nextButton.classList.add('bg-[#1DD100]');
+            nextButton.classList.add('text-white');
+            
         } else {
             nextButton.classList.add('hide');
+
         }
         
-
         // button disable if clicked again
         if (!btn.disabled) {
             btn.disabled = true;
@@ -105,16 +117,29 @@ apply.addEventListener('click', function(){
         const grantTotal = totalPrice * 0.15;
         const discountGrantTotal = totalPrice - grantTotal ;
         grandPriceElement.innerText = discountGrantTotal;
+        // discount price
+        const discountPrice = document.getElementById('discountPrice');
+        discountPrice.innerText = grantTotal;
          // clear value of input field
          document.getElementById('input-field').value = '';
+        //  hidden full coupon div
+        const inputDiv = document.getElementById('input-div');
+        inputDiv.classList.add('hidden')
+         
     }else if(convertCoupon === 'COUPLE20'){
         // grand total
         const grandPriceElement = document.getElementById('grand');
         const grantTotal = totalPrice * 0.2;
         const discountGrantTotal = totalPrice - grantTotal ;
         grandPriceElement.innerText = discountGrantTotal;
+        // discount price
+        const discountPrice = document.getElementById('discountPrice');
+        discountPrice.innerText = grantTotal;
          // clear value of input field
          document.getElementById('input-field').value = '';
+        //  hidden full coupon div
+        const inputDiv = document.getElementById('input-div');
+        inputDiv.classList.add('hidden')
     }
     else{
         alert('Invalid Coupon code')
@@ -122,6 +147,18 @@ apply.addEventListener('click', function(){
         document.getElementById('input-field').value = '';
     }
 })
+
+// if clicked next button then phone number and email will clear
+        // clear value of input field
+
+        document.getElementById('continue').addEventListener('click', function(){
+            document.getElementById('email').value = '';
+            document.getElementById('phoneNumber').value = '';
+            document.getElementById('name').value = '';
+            
+        })
+        
+
 
 // set innerText
 function setInnerText(id, value) {
@@ -131,3 +168,5 @@ function setInnerText(id, value) {
 function getInnerText(id, value) {
     document.getElementById(id).innerText = value;
 }
+
+
